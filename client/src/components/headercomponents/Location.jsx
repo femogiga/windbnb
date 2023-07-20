f4be612a5bd9015d2fd0bcb81f3919e6df3d1d79
femogiga <femogiga@yahoo.com>
2023-07-20 12:00:18 +0100

    beta

--- client/src/components/headercomponents/Location.jsx
+++ client/src/components/headercomponents/Location.jsx
@@ -8,6 +8,7 @@ import { Link } from "react-router-dom"
 
 const Location = ({ placeholder, onClick, onBlur ,isLocationExpand}) => {
     const { data } = useContext(DataContext)
+    const[loc,setLoc] = useState('Helsinki, Finland')
     const newSet = new Set(data.map(item => item.city))
 
     const newArr = Array.from(newSet).filter((item) => item !== undefined)
@@ -16,18 +17,23 @@ const Location = ({ placeholder, onClick, onBlur ,isLocationExpand}) => {
         setNewData(newArr)
     }, [])
 
-
+    const handleLocClick = (e)=>{
+        e.preventDefault()
+        setLoc(e.target.innerText)
+        console.log(loc)
+    }
 
     console.log(isLocationExpand)
     return (
         <div className={`location controls flow-2`}>
             <div className={'label-parent'}>
                 <p>LOCATION</p>
+                <label>{loc}</label>
                 <button type='text' placeholder={'Helsinki ,Finland'} onClick={onClick} onBlur={onBlur} />
             </div>
             <ul className={``}>
                 {
-                    isLocationExpand && newData.filter(item => item !== undefined).map((loc, index) => <li key={index}><Link className={'link'} key={index} to={"#"}>{<LocationCard city={loc} country={'Finland'} />}</Link></li>)
+                    isLocationExpand && newData.filter(item => item !== undefined).map((loc, index) => <li key={index}><Link onClick={handleLocClick} className={'link'} value={loc} to={"#"}>{<LocationCard city={loc} country={'Finland'} />}</Link></li>)
                 }
             </ul>
             {/* <LocationCard /> */}
